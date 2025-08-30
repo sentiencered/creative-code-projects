312d10296abaa25424419e7bc14a806a97150d73
sentiencered <124637570+sentiencered@users.noreply.github.com>
2025-08-29 21:08:01 -0500

    add noise grid
    edited p5 background
    edited css fonts

--- sketch.js
+++ sketch.js
@@ -1,11 +1,26 @@
 console.log("hello")
-function setup(){
-    createCanvas(windowWidth, windowHeight)
+
+function setup() {
+  //let s= 700
+  createCanvas(windowWidth, windowHeight);
 }
 
-function draw(){
-    background(220)
-    ellipse(mouseX, mouseY, 20, 20)
+function draw() {
+  background(130, 150, 175);
+  cursor(CROSS);
+  
+  // fill(230, 115, 75);
+  fill(75, 95, 130);
+  noStroke();
+  
+  let n= width/20;
+  let f= width/n
+  for(x=f; x<width; x+=f){
+    for(y=f; y<height; y+=f){
+      let d= f* noise((x+f), (y+f), frameCount/200);
+      ellipse(x, y, d, d);
+    }
+  }
 }
 
 function windowResized(){
