e1999b09475722a0ef34c63fa1762194420bef37
sentiencered <124637570+sentiencered@users.noreply.github.com>
2025-08-31 19:35:51 -0500

    add button
    improve formatting (add/edit css classes)
    add css buttons/hover classes
    add 2nd noise grid/change opacity

--- sketch.js
+++ sketch.js
@@ -10,11 +10,20 @@ function draw() {
   cursor(CROSS);
   
   // fill(230, 115, 75);
-  fill(75, 95, 130);
+  // fill(75, 95, 130);
   noStroke();
   
   let n= width/20;
   let f= width/n
+
+  fill(105, 125, 165, 200);
+  for(a=f; a<width; a+=f){
+    for(b=f; b<width; b+=f){
+      let o= f* noise((a+f), (b+f), frameCount/400);
+      ellipse(a, b, o, o);
+    }
+  }
+  fill(75, 95, 130, 200);
   for(x=f; x<width; x+=f){
     for(y=f; y<height; y+=f){
       let d= f* noise((x+f), (y+f), frameCount/200);
